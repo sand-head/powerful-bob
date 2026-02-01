5e33def5523960f3a43ce02165ebda9a49fb0760
Jessie Schweigert <jessie@schweigert.dev>
2026-02-01 17:05:06 -0600

    Enable terra

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,14 +8,18 @@ set -ouex pipefail
 
 dnf5 -y copr enable yalter/niri-git
 echo "priority=1" | tee -a /etc/yum.repos.d/_copr:copr.fedorainfracloud.org:yalter:niri-git.repo
-dnf -y --enablerepo copr:copr.fedorainfracloud.org:yalter:niri-git \
+dnf5 -y --enablerepo copr:copr.fedorainfracloud.org:yalter:niri-git \
     install --setopt=install_weak_deps=False \
     niri
 dnf5 -y copr disable yalter/niri-git
 
 # Install Noctalia (in terra repo)
 
+dnf5 config-manager setopt terra.enabled=1 terra-extras.enabled=1
+
 dnf5 -y install --setopt=install_weak_deps=False noctalia-shell
+
+dnf5 config-manager setopt terra.enabled=0 terra-extras.enabled=0
 
 #### Example for enabling a System Unit File
 
